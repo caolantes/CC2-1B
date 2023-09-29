03836d4be8bca63a9e496ea055fe16716370b930
caolantes <145944488+caolantes@users.noreply.github.com>
2023-09-29 03:43:12 -0700

    Add files via upload

---
+++ CC2-1B.py
@@ -0,0 +1,19 @@
+
+# Enter name
+name =  input("What is your name?")
+# Enter age
+age_input = input("What is your age?")
+
+# Convert age input into an integer
+age = int(age_input)
+
+age_next_year = age + 1
+
+pi = 3.1416
+
+print(f"Hello, (name)")
+print("Hello, "+name)
+print("Hello, " ,name, sep="%%%%")
+print(f"My age next year is (age_next_year)")
+print(f"Pi value is (pi)")
+
